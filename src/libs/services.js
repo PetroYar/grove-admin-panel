7b8bca13de93888e70d1af5a0403f4b8b1505514
PetroYar <petryarish@gmail.com>
2025-04-02 22:12:13 +0300

    Update services.js

--- src/libs/services.js
+++ src/libs/services.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const URL =
-  "https://grove-server-one.vercel.app/";
+
+const URL = "https://grove-server-one.vercel.app/api";
 // const URL = "localhost:5000/api";
 
 const getToken = () => {
